eb5b7459258edf72e3f663d122125d6b18121e2a
Aman Kumar <amankarguwal0@gmail.com>
2024-09-12 20:29:21 +0530

    feat: Add user routes and CRUD operations

---
+++ src/routes/user.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  deleteUser,
+  getAllUsers,
+  getUserById,
+  newUser,
+} from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
+
+const app = express.Router();
+
+//route - /api/v1/user/new
+app.post("/new", newUser);
+
+//route - /api/v1/user/all
+app.get("/all",adminOnly, getAllUsers);
+
+//route - /api/v1/user/dynamicId
+app.route("/:id").get(getUserById).delete(adminOnly,deleteUser);
+
+export default app;
