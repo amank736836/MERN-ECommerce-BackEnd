716d29a6d4c5c9140301a2f823614bfa8667a0f4
Aman Kumar <amankarguwal0@gmail.com>
2024-09-12 20:29:43 +0530

    feat: Add adminOnly middleware for authorization

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request } from "express";
+import { TryCatch } from "./error.js";
+import ErrorHandler from "../utils/utility-class.js";
+import { User } from "../models/user.js";
+
+// Middleware to make sure only admin is allowed
+export const adminOnly = TryCatch(async (req, res, next) => {
+  const { id } = req.query;
+  if (!id) {
+    return next(new ErrorHandler("Please provide id", 401));
+  }
+
+  const user = await User.findById(id);
+  if (!user) {
+    return next(new ErrorHandler("User not found", 401));
+  }
+
+  if (user.role !== "admin") {
+    return next(new ErrorHandler("You are not authorized", 401));
+  }
+  next();
+});
