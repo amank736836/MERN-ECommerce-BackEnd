8a5c617d1f3f98eb9b4402c47cabcb8c66891b5e
Aman Kumar <amankarguwal0@gmail.com>
2024-09-12 20:29:52 +0530

    feat: Add error middleware and TryCatch decorator
    This commit adds the error middleware and TryCatch decorator to handle errors in the application. The error middleware handles any errors thrown by the controllers and returns a JSON response with the error message and status code. The TryCatch decorator wraps the controller functions with a Promise.resolve().catch() block to catch any asynchronous errors and pass them to the error middleware.
    
    Refs: #123

---
+++ src/middlewares/error.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/utility-class.js";
+import { ControllerType } from "../types/types.js";
+
+export const errorMiddleware = (
+  err: ErrorHandler,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  return res.status(400).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    statusCode: err.statusCode || 500,
+  });
+};
+
+export const TryCatch = (func: ControllerType) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    return Promise.resolve(func(req, res, next)).catch(next);
+  };
+};
