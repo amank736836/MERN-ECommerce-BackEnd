e49b0efb4a138d3c9829bb089e8423107796f613
Aman Kumar <amankarguwal0@gmail.com>
2024-09-12 20:29:00 +0530

    feat: Add user routes and error middleware
    This commit adds user routes and error middleware to the application. It also includes the necessary imports and configurations for these features. The user routes are accessible at the "/api/v1/user" endpoint, and the error middleware handles any errors that occur during the request processing.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import userRoutes from "./routes/user.js";
+import { connectDB } from "./utils/features.js";
+import { errorMiddleware } from "./middlewares/error.js";
+const port = 4000;
 
-const port = 3000;
+connectDB();
 
 const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
+// USing Routes
+app.use("/api/v1/user", userRoutes);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
