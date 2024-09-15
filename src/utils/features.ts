20b314345f8b59b4abe2c37dc0b31a76fe6d74e9
Aman Kumar <amankarguwal0@gmail.com>
2024-09-15 22:23:01 +0530

    Refactor connectDB function and add invalidateCache function

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -1,14 +1,48 @@
 import mongoose from "mongoose";
+import { InvalidateCacheProps } from "../types/types.js";
+import { myCache } from "../app.js";
+import { Product } from "../models/product.js";
 
 export const connectDB = () => {
   mongoose
-    .connect("mongodb+srv://aman:4n1i43zhssTGimyD@e-commerce.z9v1cge.mongodb.net/", {
-      dbName: "Ecommerce_24",
-    })
+    .connect(
+      "mongodb+srv://aman:4n1i43zhssTGimyD@e-commerce.z9v1cge.mongodb.net/",
+      {
+        dbName: "Ecommerce_24",
+      }
+    )
     .then((c) => {
       console.log(`DB Connected to ${c.connection.name}`);
     })
     .catch((err) => {
       console.log(`DB Connection Error: ${err.message}`);
     });
+};
+
+export const invalidateCache = async ({
+  product,
+  order,
+  admin,
+}: InvalidateCacheProps) => {
+  if (product) {
+    const productKeys: string[] = [
+      "latest-products",
+      "categories",
+      "all-products",
+    ];
+    // myCache.keys().forEach((key) => {
+    //   productKeys.push(key);
+    // });
+    const products = await Product.find({}).select("_id");
+
+    products.forEach((product) => {
+      productKeys.push(`product-${product._id}`);
+    });
+
+    myCache.del(productKeys);
+  }
+  if (order) {
+  }
+  if (admin) {
+  }
 };
