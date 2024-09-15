31de89ae7742818305438130e4b52239c9bd1227
Aman Kumar <amankarguwal0@gmail.com>
2024-09-15 22:22:44 +0530

    Refactor types.ts and add search functionality

--- src/types/types.ts
+++ src/types/types.ts
@@ -22,3 +22,28 @@ export interface NewProductRequestBody {
   category: string;
   stock: number;
 }
+
+export type SearchRequestQuery = {
+  search?: string;
+  price?: string;
+  category?: string;
+  sort?: string;
+  page?: string;
+};
+
+export interface BaseQuery {
+  name?: {
+    $regex: string;
+    $options: string;
+  };
+  price?: {
+    $lte: number;
+  };
+  category?: string;
+}
+
+export type InvalidateCacheProps = {
+  product?: boolean;
+  order?: boolean;
+  admin?: boolean;
+};
