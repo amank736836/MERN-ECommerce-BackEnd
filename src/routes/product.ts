565e280b595e5b42997b6efe08352258d411d646
Aman Kumar <amankarguwal0@gmail.com>
2024-09-15 22:22:29 +0530

    Refactor product routes and add search functionality

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -2,12 +2,13 @@ import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
 import {
   deleteProduct,
-  getAdminProducts,
+  getAllProducts,
   getAllCategories,
   getLatestProducts,
   getSingleProduct,
   newProduct,
   updateProduct,
+  getSearchProducts,
 } from "../controllers/product.js";
 import { singleUpload } from "../middlewares/multer.js";
 
@@ -16,6 +17,9 @@ const app = express.Router();
 // Create New Product - /api/v1/product/new
 app.post("/new", adminOnly, singleUpload, newProduct);
 
+// Get Search Products with filters - /api/v1/product/search?keyword=apple&category=Electronics
+app.get("/search", getSearchProducts);
+
 // Get Latest Products - /api/v1/product/latest
 app.get("/latest", getLatestProducts);
 
@@ -23,13 +27,13 @@ app.get("/latest", getLatestProducts);
 app.get("/categories", getAllCategories);
 
 // Get All Products - /api/v1/product/admin-products
-app.get("/admin-products", getAdminProducts);
+app.get("/admin-products", adminOnly, getAllProducts);
 
 // Get Single Product - /api/v1/product/:id
 app
   .route("/:id")
   .get(getSingleProduct)
-  .put(singleUpload, updateProduct)
-  .delete(deleteProduct);
+  .put(adminOnly, singleUpload, updateProduct)
+  .delete(adminOnly, deleteProduct);
 
 export default app;
