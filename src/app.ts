ba425e2ffebcb75a47244a166b8dc8488b3bc92f
Aman Kumar <amankarguwal0@gmail.com>
2024-09-18 18:28:55 +0530

    Refactor app.ts to add logging and new routes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,28 @@
 import express from "express";
-import userRoutes from "./routes/user.js";
-import productRoutes from "./routes/product.js";
-import { connectDB } from "./utils/features.js";
-import { errorMiddleware } from "./middlewares/error.js";
 import NodeCache from "node-cache";
+import { errorMiddleware } from "./middlewares/error.js";
+import orderRoutes from "./routes/order.js";
+import productRoutes from "./routes/product.js";
+import userRoutes from "./routes/user.js";
+import paymentRoutes from "./routes/payment.js";
+import dashboardRoute from "./routes/stats.js";
+import { connectDB } from "./utils/features.js";
+import { config } from "dotenv";
+import morgan from "morgan";
 
-const port = 4000;
+config({
+  path: "./.env",
+});
 
-connectDB();
+const port = process.env.PORT || 3000;
+
+connectDB(process.env.MONGO_URI as string);
 
 export const myCache = new NodeCache();
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("API is running....");
@@ -22,6 +32,12 @@ app.get("/", (req, res) => {
 app.use("/api/v1/user", userRoutes);
 
 app.use("/api/v1/product", productRoutes);
+
+app.use("/api/v1/order", orderRoutes);
+
+app.use("/api/v1/payment", paymentRoutes);
+
+app.use("/api/v1/dashboard", dashboardRoute);
 
 app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
