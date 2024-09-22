15ac58e8683828cdf3629e08195acb6bf3c13b70
Aman Kumar <amankarguwal0@gmail.com>
2024-09-22 23:29:48 +0530

    Refactor auth middleware to add admin cache check

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,3 +1,4 @@
+import { myCache } from "../app.js";
 import { User } from "../models/user.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { TryCatch } from "./error.js";
@@ -9,6 +10,10 @@ export const adminOnly = TryCatch(async (req, res, next) => {
     return next(new ErrorHandler("Please provide admin id", 401));
   }
 
+  if (myCache.has(`${id}-admin`)) {
+    return next();
+  }
+
   const user = await User.findById(id);
   if (!user) {
     return next(new ErrorHandler("User not found", 401));
@@ -17,5 +22,8 @@ export const adminOnly = TryCatch(async (req, res, next) => {
   if (user.role !== "admin") {
     return next(new ErrorHandler("You are not authorized", 403));
   }
+
+  myCache.set(`${id}-admin`, true);
+
   next();
 });
