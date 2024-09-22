c00ac708c83c8413029910dfadfbe9930321005a
Aman Kumar <amankarguwal0@gmail.com>
2024-09-22 23:29:08 +0530

    Refactor stats controller and add routes for dashboard charts

--- src/routes/stats.ts
+++ src/routes/stats.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import { adminOnly } from "../middlewares/auth.js";
+import {
+  getBarCharts,
+  getDashboardStats,
+  getLineCharts,
+  getPieCharts,
+} from "../controllers/stats.js";
 
 const app = express.Router();
 
 // Get the stats - /api/v1/dashboard/stats
-app.get("/stats", adminOnly);
+app.get("/stats", adminOnly, getDashboardStats);
 
 // Get the pie chart data - /api/v1/dashboard/pie
-app.get("/pie", adminOnly);
+app.get("/pie", adminOnly, getPieCharts);
 
 // Get the bar chart data - /api/v1/dashboard/bar
-app.get("/bar", adminOnly);
+app.get("/bar", adminOnly, getBarCharts);
 
 // Get the line chart data - /api/v1/dashboard/line
-app.get("/line", adminOnly);
+app.get("/line", adminOnly, getLineCharts);
 
 export default app;
