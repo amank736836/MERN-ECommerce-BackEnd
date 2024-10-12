fe19ff17bfbbedd3928d5a58aac9b12adfe8a538
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 17:59:56 +0530

    Refactor multer.ts to create uploads directory if it doesn't exist

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,7 +1,11 @@
 import fs from "fs";
 import multer from "multer";
 import path from "path";
+import { fileURLToPath } from "url";
 import { v4 as uuid } from "uuid";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const uploadsDir = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadsDir)) {
