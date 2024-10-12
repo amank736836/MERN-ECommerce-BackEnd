be12d57813173f543d962f3c9cf132fde0948fe4
Aman Kumar <144197075+amank736836@users.noreply.github.com>
2024-10-12 17:08:35 +0530

    Delete dist directory

--- dist/controllers/notFound.js
+++
@@ -1,8 +0,0 @@
-import { TryCatch } from "../middlewares/error.js";
-const notFound = TryCatch(async (req, res, next) => {
-    res.status(404).json({
-        success: false,
-        message: "Resource not found",
-    });
-});
-export default notFound;
--- dist/src/routes/user.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { newUser } from "../controllers/user.js";
-const app = express.Router();
-app.post("/new", newUser);
-export default app;
