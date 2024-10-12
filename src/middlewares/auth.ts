6275c573718b82a9aa50b817c9aa1325f73fed35
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 13:26:35 +0530

    Refactor auth middleware to remove unused code

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,7 +3,6 @@ import { User } from "../models/user.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { TryCatch } from "./error.js";
 
-// Middleware to make sure only admin is allowed
 export const adminOnly = TryCatch(async (req, res, next) => {
   const { id } = req.query;
   if (!id) {
