b17e8603b4bf7f44dc96156886beb9ebab49019f
Aman Kumar <amankarguwal0@gmail.com>
2024-10-12 16:55:37 +0530

    Refactor app.ts to include CORS policy with additional allowed origins

--- dist/app.js
+++ dist/app.js
@@ -12,7 +12,7 @@ import dashboardRoute from "./routes/stats.js";
 import userRoutes from "./routes/user.js";
 import { connectDB } from "./utils/features.js";
 config({
-  path: "./.env",
+    path: "./.env",
 });
 import Stripe from "stripe";
 const stripeKey = process.env.STRIPE_KEY || "";
@@ -20,8 +20,8 @@ export const stripe = new Stripe(stripeKey);
 import Razorpay from "razorpay";
 import ErrorHandler from "./utils/utility-class.js";
 export const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID || "",
-  key_secret: process.env.RAZORPAY_KEY_SECRET || "",
+    key_id: process.env.RAZORPAY_KEY_ID || "",
+    key_secret: process.env.RAZORPAY_KEY_SECRET || "",
 });
 const port = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || "";
@@ -30,26 +30,21 @@ export const myCache = new NodeCache();
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://mern-ecommerce-2024-ak.firebaseapp.com/",
-];
-app.use(
-  cors({
+const allowedOrigins = ["http://localhost:5173"];
+app.use(cors({
     origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new ErrorHandler(msg, 403), false);
-      }
-      return callback(null, true);
+        if (!origin)
+            return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+            return callback(new ErrorHandler(msg, 403), false);
+        }
+        return callback(null, true);
     },
     credentials: true,
-  })
-);
+}));
 app.get("/", (req, res) => {
-  res.send("API is running....");
+    res.send("API is running....");
 });
 // USing Routes
 app.use("/api/v1/user", userRoutes);
@@ -61,5 +56,5 @@ app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
 app.use("*", NotFound);
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
