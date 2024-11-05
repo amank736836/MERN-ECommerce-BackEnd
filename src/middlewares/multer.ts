2ad1c6bfc8112237d1dc71622bff1c8c4a7c8626
Aman Kumar <amankarguwal0@gmail.com>
2024-11-05 09:35:53 +0530

    Refactor uploads directory path in multer middleware

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -3,13 +3,11 @@ import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
 import { v4 as uuid } from "uuid";
-import { errorMiddleware } from "./error.js";
-import ErrorHandler from "../utils/utility-class.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const uploadsDir = path.join(__dirname, "uploads");
+const uploadsDir = path.join("./uploads");
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true });
 }
