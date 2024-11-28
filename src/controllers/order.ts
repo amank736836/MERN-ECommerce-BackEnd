5231318a90cd030cf72f1736a7e6ecc74b7bae18
Aman Kumar <amankarguwal0@gmail.com>
2024-11-29 00:31:07 +0530

    Refactor order controller to use Redis for caching and improve order retrieval logic

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,44 +1,26 @@
 import { Request } from "express";
+import { redis } from "../app.js";
 import { TryCatch } from "../middlewares/error.js";
-import { NewOrderRequestBody } from "../types/types.js";
 import { Order } from "../models/order.js";
-import { invalidateCache, reduceStock } from "../utils/features.js";
-import ErrorHandler from "../utils/utility-class.js";
-import { myCache, razorpay } from "../app.js";
+import { Payment } from "../models/payment.js";
 import { Product } from "../models/product.js";
 import { User } from "../models/user.js";
-import { Payment } from "../models/payment.js";
-
-export const myOrders = TryCatch(async (req, res, next) => {
-  const { id: user } = req.query;
-
-  const key = `my-orders-${user}`;
-
-  let orders = [];
-
-  if (myCache.has(key)) {
-    orders = JSON.parse(myCache.get(key) as string);
-  } else {
-    orders = await Order.find({ user }).populate("user", "name");
-    myCache.set(key, JSON.stringify(orders));
-  }
-
-  return res.status(200).json({
-    success: true,
-    message: "My Orders",
-    orders,
-  });
-});
+import { NewOrderRequestBody } from "../types/types.js";
+import { invalidateCache, reduceStock } from "../utils/features.js";
+import ErrorHandler from "../utils/utility-class.js";
 
 export const getAllOrders = TryCatch(async (req, res, next) => {
   const key = `all-orders`;
+
   let orders = [];
 
-  if (myCache.has(key)) {
-    orders = JSON.parse(myCache.get(key) as string);
+  const cachedData = await redis.get(key);
+
+  if (cachedData) {
+    orders = JSON.parse(cachedData);
   } else {
     orders = await Order.find().populate("user", "name");
-    myCache.set(key, JSON.stringify(orders));
+    await redis.set(key, JSON.stringify(orders));
   }
 
   return res.status(200).json({
@@ -48,19 +30,54 @@ export const getAllOrders = TryCatch(async (req, res, next) => {
   });
 });
 
+export const myOrders = TryCatch(async (req, res, next) => {
+  const { id: user } = req.query;
+
+  if (!user) {
+    return next(new ErrorHandler("Please enter a user id", 400));
+  }
+
+  const key = `my-orders-${user}`;
+
+  let orders = [];
+
+  const cachedData = await redis.get(key);
+
+  if (cachedData) {
+    orders = JSON.parse(cachedData);
+  } else {
+    orders = await Order.find({ user }).populate("user", "name");
+    await redis.set(key, JSON.stringify(orders));
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "My Orders",
+    orders,
+  });
+});
+
 export const getSingleOrder = TryCatch(async (req, res, next) => {
   const { id } = req.params;
+
+  if (!id) {
+    return next(new ErrorHandler("Please enter an order id", 400));
+  }
+
   const key = `order-${id}`;
+
   let order;
 
-  if (myCache.has(key)) {
-    order = JSON.parse(myCache.get(key) as string);
+  const cachedData = await redis.get(key);
+
+  if (cachedData) {
+    order = JSON.parse(cachedData);
   } else {
     order = await Order.findById(req.params.id).populate("user", "name");
     if (!order) {
       return next(new ErrorHandler("Order not found", 404));
     }
-    myCache.set(key, JSON.stringify(order));
+    await redis.set(key, JSON.stringify(order));
   }
 
   return res.status(200).json({
@@ -141,7 +158,6 @@ export const processOrder = TryCatch(async (req, res, next) => {
   await order.save();
 
   invalidateCache({
-    product: false,
     order: true,
     admin: true,
     userId: order.user,
@@ -184,15 +200,6 @@ export const cancelOrder = TryCatch(async (req, res, next) => {
 
   await order.save();
 
-  invalidateCache({
-    product: true,
-    order: true,
-    admin: true,
-    userId: order.user,
-    orderId: String(order._id),
-    productId: order.orderItems.map((item) => item.productId.toString()),
-  });
-
   for (let i = 0; i < order.orderItems.length; i++) {
     const item = order.orderItems[i];
     const product = await Product.findById(item.productId);
@@ -202,6 +209,15 @@ export const cancelOrder = TryCatch(async (req, res, next) => {
     product.stock = product.stock + item.quantity;
     await product.save();
   }
+
+  invalidateCache({
+    product: true,
+    order: true,
+    admin: true,
+    userId: order.user,
+    orderId: String(order._id),
+    productId: order.orderItems.map((item) => item.productId.toString()),
+  });
 
   return res.status(200).json({
     success: true,
@@ -217,7 +233,7 @@ export const deleteOrder = TryCatch(async (req, res, next) => {
     return next(new ErrorHandler("Order not found", 404));
   }
 
-  if (order.status === "Shipped") {
+  if (order.status === "Shipped" || order.status === "Processing") {
     return next(new ErrorHandler("You cannot delete this order", 400));
   }
 
@@ -230,8 +246,19 @@ export const deleteOrder = TryCatch(async (req, res, next) => {
   await payment.deleteOne();
   await order.deleteOne();
 
+  if (order.status !== "Cancelled" && order.status !== "Delivered") {
+    for (let i = 0; i < order.orderItems.length; i++) {
+      const item = order.orderItems[i];
+      const product = await Product.findById(item.productId);
+      if (!product) {
+        return next(new ErrorHandler("Product not found", 404));
+      }
+      product.stock = product.stock + item.quantity;
+      await product.save();
+    }
+  }
+
   invalidateCache({
-    product: false,
     order: true,
     admin: true,
     userId: order.user,
